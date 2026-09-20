ccbfd0b698a1d6a5c9a72cf083fd8eb0ed88b316
Anthony Hatfield <5w7rzb5jsj@privaterelay.appleid.com>
2026-09-20 00:42:13 -0400

    Add benchmark for throughput testing

---
+++ ingester/throughput_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"testing"
+)
+
+func BenchmarkThroughput(b *testing.B) {
+	const lines = 100000
+
+	input := strings.Repeat("event test data\n", lines)
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		scanner := bufio.NewScanner(strings.NewReader(input))
+
+		for scanner.Scan() {
+			_, _ = io.Discard.Write([]byte(scanner.Bytes()))
+		}
+
+		if err := scanner.Err(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
